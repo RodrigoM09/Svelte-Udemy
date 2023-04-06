a0758c444c3115207b30212f353f2afc844ae589
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-04-05 19:20:48 -0500

    autosave script

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,12 @@
 <script>
 let name = 'svelte';
+let src = 'https://picsum.photos/200/300';
+let string = 'Some <strong>HTML</strong> string';
 </script>
 
+<img {src} alt="">
 <h1>Hello {name.toUpperCase()}</h1>
-
+<p>{@html string}</p>
 <style>
 h1 {
   color: red;
